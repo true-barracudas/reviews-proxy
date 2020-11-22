3c0ad14c9ae0a0879e790359b7aca2efafb2bbf9
meekoder <meekorusdi@gmail.com>
2020-11-21 18:20:32 -0800

    completed proxy server

--- server/server.js
+++ server/server.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.static(PUBLIC_DIR));
 
+app.get('/:id', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
 // Handling asset requests for webpack bundles by passing off requests to the bundles router
 app.use('/bundles', router.bundles);
 // Handling AJAX requests to the API by passing off requests to the api router
